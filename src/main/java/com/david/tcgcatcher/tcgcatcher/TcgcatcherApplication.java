7dd69bb9382098c6437c0b8c0a211d36c7775abf
David <davidcarmonaarrabal18@gmail.com>
2024-11-16 17:15:07 +0100

    Initial commit, project created

---
+++ src/main/java/com/david/tcgcatcher/tcgcatcher/TcgcatcherApplication.java
@@ -0,0 +1,13 @@
+package com.david.tcgcatcher.tcgcatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcgcatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcgcatcherApplication.class, args);
+	}
+
+}
